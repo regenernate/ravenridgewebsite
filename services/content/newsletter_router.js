18bf08fcddca1d5cde8359ad17a289ad2fb16ea0
Nathan Wheeler <nathangreenling@gmail.com>
2021-01-21 07:12:32 -0500

    displaying newsletter

--- services/content/newsletter_router.js
+++ services/content/newsletter_router.js
@@ -8,6 +8,8 @@ It is used here to load the general information pages for the RRHH website
 
 
 const fs = require('fs');
+const fetch = require('node-fetch');
+
 //The server will load active routers and if a request comes in with the base_route_path as specified below,
 //the server will call the defined router method as below, passing along the request and response objects,
 //and an array of the remaining url parts as separated by "/"
@@ -20,13 +22,121 @@ module.exports.active = true;
 //load the basic return object prototype
 var bro = require("../../server/bro");
 //load any controllers needed to handle requests
+
+
 var template_manager = require('../../services/template_manager');
+var templates = {};
+var pages = { home:{
+  template:'./services/content/views/newsletter_home.handlebars',
+  title:'This is the newsletter homepage!',
+  desc:'The RavenRidge family farm newsletter is intended to provide important information about regenerative health and agriculture.'
+  },
+  newsletter:{
+    template:'./services/content/views/newsletter_view.handlebars',
+    title:'This is the default newsletter title',
+    desc:'This is a description for this newsletter.'
+  }
+};
+
+compileTemplates();
+//this method is called after pages.json loads
+function compileTemplates(){
+  //extract just template name/path pairs for template manager to compile from
+  for( var i in pages ){
+    templates[i] = pages[i].template;
+  }
+  template_manager.compileTemplates(templates, true);
+}
+
+const dib_token = "b=HVGYH9CBTW48PTWHQ2ET";
+const SUCCESS = "success";
+var newsletter_list = [{slug:'january-2021', title:'January 2021 - Now introducing ...'}];
+var sections={};
+
+loadNewsletterPosts();
+
+async function loadNewsletterPosts(){
+  let json;
+  try {
+    let response = await fetch('https://api.dropinblog.com/v1/json/?' + dib_token + '&category=newsletter');
+    json = await response.json();
+    if(json && json.status == SUCCESS ){
+    //  console.log(sections);
+      await constructEditions( json.data.posts );
+      //now we need to load the content for this newsletter
+
+    }else{
+      throw new Error("Remote blog posts call failed");
+    }
+//    console.log(post_listing);
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+  return true;
+}
+
+async function constructEditions( content ){
+  for( let i=0; i<newsletter_list.length; i++ ){
+    //initialize the sections of this newsletter edition
+    newsletter_list[i].sections = { narrative:"", "our-backyards":"", "regenerative-health":"" };
+    //get this editions slug for quick access
+    let this_edition = newsletter_list[i].slug;
+    sections[ this_edition ] = {};
+    //find the sections of this edition
+    for( let j in content ){
+//      console.log("looking for ", this_edition );
+      let belongs = false;
+      //check categories to see if this content belongs in the current newsletter
+      for( let k=0; k<content[j].categories.length; k++ ){
+        //strip off the "newsletter-" part
+        let tmp_slug = content[j].categories[k].slug.split("-");
+        if( tmp_slug[0] == "newsletter" ) tmp_slug.shift();
+        let section = tmp_slug.join("-");
+        if( !section ) continue; //skip the base "newsletter" category
+//        console.log("   is there a ", section);
+        if( !belongs && section == this_edition ){ //this is the newsletter category and matches the current edition
+          belongs = true;
+//          console.log("Yayy!!! it belongs");
+          k = -1; //reset counter since we now know this content belongs to the edition we are looking for
+          continue;
+        }else if( belongs ){ //this is one of the subsection categories OR the base "newsletter" category which can be ignored
+          if( newsletter_list[i].sections.hasOwnProperty( section ) ){
+//            console.log("   we found it!!!!");
+            newsletter_list[i].sections[ section ] = content[j];
+            //load this actual content
+            let fsc = 'https://api.dropinblog.com/v1/json/post/?' + dib_token + "&post=" + content[j].slug;
+            let response = await fetch(fsc);
+            json = await response.json();
+            if(json && json.status == SUCCESS ) sections[ this_edition ][ section ] = json.data;
+            else console.log( "Newsletter Router could not load post ", content[j].slug );
+          }
+        }
+      }
+    }
+//    console.log( "list of newsletters :: ",  newsletter_list );
+//    console.log( "list of content :: ", sections );
+  }
+}
 
 //write a method to handle route requests and return a bro
 async function routeRequest( request, response, file_parts ){
   let rtn = null;
+  let template;
   //use default error message for now
-  rtn = bro.get(true, template_manager.executeTemplate( template_manager.unsupported_route, {nav:{newsletter:true}, message:"Our newsletter will be implemented mid-January."} ) );
+  if( !file_parts || !file_parts.length ){
+    file_parts[0] = "home";
+  }
+  let data_to_send = {nav:{newsletter:true}};
+  data_to_send.newsletters = newsletter_list; //send the list of newsletters to every page in this domain
+  if( file_parts[0] == "home" || !sections.hasOwnProperty( file_parts[0] ) ){
+    template = "home";
+  }else{
+    template = "newsletter";
+    data_to_send.title = "January 2021 - Introducing the newsletter."
+    data_to_send.sections = sections[ file_parts[0] ];
+  }
+  rtn = bro.get(true, template_manager.executeTemplate( templates[template], data_to_send ) );
   return rtn;
 }
 
