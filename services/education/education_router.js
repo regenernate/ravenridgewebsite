29171ccdef5dc188adf26afcaf7aa87552163fc8
Nathan Wheeler <nathangreenling@gmail.com>
2021-03-16 10:06:54 -0400

    cleaned up testing and pricing pages

--- services/education/education_router.js
+++ services/education/education_router.js
@@ -108,10 +108,10 @@ var pricing = {
       strength:"1%",
       sizes:[
         {
-          size:"1", total_cbd:"300", full_price:"42", subscription_price:"35", full_mg_price:".14", subscription_mg_price:".12"
+          size:"1", total_cbd:"300", full_price:"39", subscription_price:"35", full_mg_price:".13", subscription_mg_price:".12"
         },
         {
-          size:"2", total_cbd:"600", full_price:"56", subscription_price:"48", full_mg_price:".09", subscription_mg_price:".08"
+          size:"2", total_cbd:"600", full_price:"55", subscription_price:"48", full_mg_price:".09", subscription_mg_price:".08"
         },
         {
           size:"4", total_cbd:"1200", full_price:"88", subscription_price:"76", full_mg_price:".07", subscription_mg_price:".06"
@@ -121,10 +121,10 @@ var pricing = {
       strength:"2%",
       sizes:[
         {
-          size:"1", total_cbd:"600", full_price:"50", subscription_price:"42", full_mg_price:".08", subscription_mg_price:".07"
+          size:"1", total_cbd:"600", full_price:"49", subscription_price:"42", full_mg_price:".08", subscription_mg_price:".07"
         },
         {
-          size:"2", total_cbd:"1200", full_price:"88", subscription_price:"74", full_mg_price:".07", subscription_mg_price:".06"
+          size:"2", total_cbd:"1200", full_price:"82", subscription_price:"75", full_mg_price:".07", subscription_mg_price:".06"
         } /*         {
                   size:"4", total_cbd:"2400", full_price:"130", subscription_price:"110", full_mg_price:".054", subscription_mg_price:".046"
                 }
@@ -134,16 +134,16 @@ var pricing = {
   ],
   sublingual:[
     {
-      strength:"1%", total_cbd:"300", full_price:"28", subscription_price:"24", full_mg_price:".09", subscription_mg_price:".08"
+      strength:"1%", total_cbd:"300", full_price:"27", subscription_price:"24", full_mg_price:".09", subscription_mg_price:".08"
     },
     {
-      strength:"2%", total_cbd:"600", full_price:"42", subscription_price:"36", full_mg_price:".07", subscription_mg_price:".06"
+      strength:"2%", total_cbd:"600", full_price:"40", subscription_price:"36", full_mg_price:".07", subscription_mg_price:".06"
     },
     {
-      strength:"3%", total_cbd:"900", full_price:"56", subscription_price:"48", full_mg_price:".06", subscription_mg_price:".05"
+      strength:"3%", total_cbd:"900", full_price:"54", subscription_price:"48", full_mg_price:".06", subscription_mg_price:".05"
     },
     {
-      strength:"4%", total_cbd:"1200", full_price:"65", subscription_price:"56", full_mg_price:".054", subscription_mg_price:".047"
+      strength:"4%", total_cbd:"1200", full_price:"63", subscription_price:"56", full_mg_price:".05", subscription_mg_price:".047"
     }
   ]
 }
