a780f46a3e2fa4d720888f51580f31cd6749a81a
Nathan Wheeler <nathangreenling@gmail.com>
2021-04-14 16:30:29 -0400

    added faqs pages for clinicians and non-clinicians

--- services/template_manager.js
+++ services/template_manager.js
@@ -81,6 +81,7 @@ handlebars.registerPartial('navigation', handlebars.compile( fs.readFileSync( ".
 handlebars.registerPartial('mtw_navigation', handlebars.compile( fs.readFileSync( "./views/shared/mtw_navigation.handlebars", 'utf-8' )));
 handlebars.registerPartial('footer', handlebars.compile( fs.readFileSync( "./views/shared/footer.handlebars", 'utf-8' )));
 handlebars.registerPartial('fda_approved', handlebars.compile( fs.readFileSync( "./views/shared/fda_approved.handlebars", 'utf-8' )));
+handlebars.registerPartial('clinician_faq', handlebars.compile( fs.readFileSync( "./views/shared/clinician_faq.handlebars", 'utf-8' )));
 
 //load and compile layout templates
 var default_layout = "logged_out";
